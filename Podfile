6cf87babb77d718f9b26e97825a66ae135eb0a35
zjade <zjade@outlook.com>
2019-10-25 23:43:25 +0800

    Create Project

---
+++ Podfile
@@ -0,0 +1,14 @@
+source 'https://github.com/CocoaPods/Specs.git'
+
+platform :ios, '10.0'
+use_frameworks!
+#Framework
+
+install! 'cocoapods', deterministic_uuids: false, generate_multiple_pod_projects: true
+
+target:'RxNetwork' do
+  pod 'Alamofire', :git => 'https://github.com/Alamofire/Alamofire'
+  pod 'RxSwift', :git => 'https://github.com/ReactiveX/RxSwift'
+end
+
+
---
+++ RxNetwork/RxNetwork.h
@@ -0,0 +1,19 @@
+//
+//  RxNetwork.h
+//  RxNetwork
+//
+//  Created by 郑军铎 on 2019/10/25.
+//  Copyright © 2019 郑军铎. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RxNetwork.
+FOUNDATION_EXPORT double RxNetworkVersionNumber;
+
+//! Project version string for RxNetwork.
+FOUNDATION_EXPORT const unsigned char RxNetworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxNetwork/PublicHeader.h>
+
+
